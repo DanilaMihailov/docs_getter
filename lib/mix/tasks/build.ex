a0e0291578acd4111c7d301c27784de5ee59857d
Danila Mihailov <danilamihailov@icloud.com>
2020-02-12 16:39:15 +0500

    add docs, moved files

--- lib/mix/tasks/build.ex
+++ lib/mix/tasks/build.ex
@@ -1,22 +1,11 @@
-defmodule Mix.Tasks.Docs.Fetch do
-  use Mix.Task
-
-  @shortdoc "Calls mix hex.docs fetch for all dependencies"
-  def run(_opts) do
-    project = Mix.Project.get()
-    deps = Path.wildcard("deps/*") |> Enum.map(fn p -> Path.split(p) |> Enum.at(1) end)
-
-    Enum.each(deps, fn name ->
-      Mix.shell().info("Getting docs for #{name}")
-      Mix.Tasks.Hex.Docs.run(["fetch", name])
-    end)
-  end
-end
-
 defmodule Mix.Tasks.Docs.Build do
   use Mix.Task
 
+  @moduledoc """
+  Builds docs for all dependencies
+  """
   @shortdoc "Builds docs for all dependencies"
+  @doc false
   def run(_opts) do
     project = Mix.Project.get()
     path = "../../doc_deps"
---
+++ lib/mix/tasks/fetch.ex
@@ -0,0 +1,17 @@
+defmodule Mix.Tasks.Docs.Fetch do
+  use Mix.Task
+  @moduledoc """
+  Calls mix hex.docs fetch for all dependencies
+  """
+  @shortdoc "Calls mix hex.docs fetch for all dependencies"
+  @doc false
+  def run(_opts) do
+    project = Mix.Project.get()
+    deps = Path.wildcard("deps/*") |> Enum.map(fn p -> Path.split(p) |> Enum.at(1) end)
+
+    Enum.each(deps, fn name ->
+      Mix.shell().info("Getting docs for #{name}")
+      Mix.Tasks.Hex.Docs.run(["fetch", name])
+    end)
+  end
+end
