b29fe5c66ce1485b91ed7a7752631680f2b449ce
Danila Mihailov <danilamihailov@icloud.com>
2020-02-13 11:01:43 +0500

    changed version to beta

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DocsGetter.MixProject do
   def project do
     [
       app: :docs_getter,
-      version: "0.1.0",
+      version: "0.1.0-beta.1",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
